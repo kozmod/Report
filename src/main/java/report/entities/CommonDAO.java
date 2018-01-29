105458767bf409ffb594152780ca6bc5fa8a2f75
kozmo <kozmod@bk.ru>
2018-01-29 21:38:15 +0300

    29/01/2018 - delete ID-interface -> became better x2

--- src/main/java/report/entities/CommonDAO.java
+++ src/main/java/report/entities/CommonDAO.java
@@ -18,7 +18,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public interface CommonDAO<C> extends TableDataBaseName{
-    <E>E getData();
+    <E> E getData();
     void delete(C entry);
     void insert(C  entry);
     /**
@@ -30,15 +30,19 @@ public interface CommonDAO<C> extends TableDataBaseName{
         this.insert(memento.toInsert());
     }
     /**
-     * Get <b>Unique</b> quantity of column.
+     * Get <b>Unique</b> distinct value of column.
      * @return ObservableList
      */
     default <X> ObservableList<X> getDistinct(String column, X ... fistNodes){
-        Set<X> disSet = new TreeSet<>(Arrays.asList(fistNodes));
+        Set<X> disSet = new TreeSet<>(
+                Arrays.asList(fistNodes)
+        );
         try (Connection connection = SQLconnector.getInstance();
-             PreparedStatement pstmt = connection.prepareStatement("execute getListDIST ?,?")){
+             PreparedStatement pstmt = connection.prepareStatement("execute getListDIST ?,?")
+        ){
             pstmt.setString(1, column);
             pstmt.setString(2, this.sqlTableName());
+//            pstmt.setString(2, this.sqlTableName());
             pstmt.execute();
             try ( ResultSet resSet = pstmt.getResultSet()){
                 while(resSet.next()){
@@ -53,6 +57,29 @@ public interface CommonDAO<C> extends TableDataBaseName{
         return  FXCollections.observableArrayList(disSet);
     }
 
-
+    /**
+     * Get <b>Unique</b> distinct value of column use SQL Table Name & SQL Table Column Name.
+     * @return ObservableList
+     */
+    default <X> ObservableList<X> getDistinct(String table,String column){
+        Set<X> disSet = new TreeSet<>( );
+        try (Connection connection = SQLconnector.getInstance();
+             PreparedStatement pstmt = connection.prepareStatement("execute getListDIST ?,?")
+        ){
+            pstmt.setString(1, column);
+            pstmt.setString(2, table);
+            pstmt.execute();
+            try ( ResultSet resSet = pstmt.getResultSet()){
+                while(resSet.next()){
+                    if(!(resSet.getObject(column) == null)
+                            && !(resSet.getObject(column).equals(ServiceStrings.Line)))
+                        disSet.add((X) resSet.getObject(column));
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(CommonDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return  FXCollections.observableArrayList(disSet);
+    }
 
 }
