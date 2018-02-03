d174af026017f1734c5376d789ec21ee5aafac4c
kozmo <kozmod@bk.ru>
2018-02-03 14:59:24 +0300

    02/02/2018 - jdbc

--- src/main/java/report/entities/items/counterparties/CounterpatiesDaoUtil.java
+++ src/main/java/report/entities/items/counterparties/CounterpatiesDaoUtil.java
@@ -6,11 +6,11 @@ import java.util.Map;
 
 public class CounterpatiesDaoUtil implements DaoUtil<String,Integer> {
     public static Map<String,Integer> getForm(){
-        String sql ="SELECT DISTINCT [id_Form],[Name] FROM [dbo].[DIC_Count_Form] WHERE dell = 0";
-        return new CounterpatiesDaoUtil().getPairValue("Name","id_Form",sql);
+        String sql ="SELECT DISTINCT [id],[Name] FROM [dbo].[DIC_Count_Form] WHERE dell = 0";
+        return new CounterpatiesDaoUtil().getPairValue("Name","id",sql);
     }
     public static Map<String,Integer> getType(){
-        String sql ="SELECT DISTINCT [id_Type],[Name] FROM [dbo].[DIC_Count_Type] WHERE dell = 0";
-        return new CounterpatiesDaoUtil().getPairValue("Name","id_Type",sql);
+        String sql ="SELECT DISTINCT [id],[Name] FROM [dbo].[DIC_Count_Type] WHERE dell = 0";
+        return new CounterpatiesDaoUtil().getPairValue("Name","id",sql);
     }
 }
