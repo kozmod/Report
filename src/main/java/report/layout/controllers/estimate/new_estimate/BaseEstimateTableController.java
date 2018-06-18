6c62ac97ae6d640f7e369a654d2a53d0c869f88a
kozmo <kozmod@bk.ru>
2018-06-18 20:12:45 +0300

    ContextMenu

--- src/main/java/report/layout/controllers/estimate/new_estimate/BaseEstimateTableController.java
+++ src/main/java/report/layout/controllers/estimate/new_estimate/BaseEstimateTableController.java
@@ -2,10 +2,13 @@ package report.layout.controllers.estimate.new_estimate;
 
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
-import javafx.collections.ObservableList;
+import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TitledPane;
@@ -17,8 +20,12 @@ import report.layout.controllers.estimate.new_estimate.service.EstimateService;
 import report.models.converters.numberStringConverters.DoubleStringConverter;
 import report.models.counterpaties.DocumentType;
 import report.models.mementos.TableMemento;
+
+import report.models.view.customNodes.newNode.ContextMenuController;
 import report.models.view.nodesFactories.TableCellFactory;
+import report.spring.spring.components.ApplicationContextService;
 import report.spring.utils.FxTableUtils;
+import report.spring.views.ControlFx;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -31,6 +38,11 @@ public class BaseEstimateTableController implements Initializable,ComtaineSumPro
 
     @Autowired
     private EstimateService estimateService;
+//    @Autowired
+//    private ApplicationContextService context;
+    @Autowired
+    private ControlFx<ContextMenu,ContextMenuController> contextMenu;
+
 
     @FXML
     private  Label sumLabel;
@@ -50,6 +62,7 @@ public class BaseEstimateTableController implements Initializable,ComtaineSumPro
                 estimateService.getEstimateMap(documentType).get(title)
         );
         computeColumnValue();
+        initTableContextMenu();
     }
 
 
@@ -124,6 +137,55 @@ public class BaseEstimateTableController implements Initializable,ComtaineSumPro
         FxTableUtils.setTextFieldTableCell(new DoubleStringConverter(),quantityColumn);
         FxTableUtils.setTextFieldTableCell(new DoubleStringConverter(),periceOneColumn);
         FxTableUtils.setTextFieldTableCell(new DoubleStringConverter(),priceOneSumColumn);
+
+    }
+
+    private void initTableContextMenu(){
+        ContextMenu contextMenu = new ContextMenu();
+
+        MenuItem add = new MenuItem("Добавить строку");
+        add.setOnAction(event -> {
+//            ViewFx<GridPane,AddEstimateRowController> addEstimateView = context.getBean("addEstimateRowView");
+//            FxStageUtils.newStage(addEstimateView.getView()).show();
+
+        });
+
+        MenuItem delete = new MenuItem("Удалить строку");
+        delete.setOnAction(event -> {
+            System.out.println("Удалить");
+        });
+        MenuItem save = new MenuItem("Сохранить");
+        save.setOnAction(event -> {
+            System.out.println("Сохранить");
+        });
+        save.setDisable(true);
+
+        MenuItem undo = new MenuItem("Отменить изменения");
+        undo.setOnAction(event -> {
+            System.out.println("Отменить изменения");
+            save.setDisable(true);
+            undo.setDisable(true);
+        });
+        undo.setDisable(true);
+
+        contextMenu.getItems().addAll(
+                add,
+                delete,
+                new SeparatorMenuItem(),
+                save,
+                undo
+        );
+
+        tableView.setContextMenu(contextMenu);
+        tableView.getItems().addListener((ListChangeListener<EstimateTVI>) observable -> {
+            if (observable.next()) {
+                if (observable.wasUpdated() || observable.wasAdded() || observable.wasRemoved()) {
+                    save.setDisable(false);
+                    undo.setDisable(false);
+                }
+            }
+        });
+
     }
 
 
